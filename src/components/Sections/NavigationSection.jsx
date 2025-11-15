2621abd71b0ede41b83d3d5e215e0bf572352fb6
codingnekochan <ibrahimoyiza198@gmail.com>
2025-11-15 11:11:37 +0100

    move assets

--- src/components/Sections/NavigationSection.jsx
+++ src/components/Sections/NavigationSection.jsx
@@ -63,7 +63,7 @@ function NavigationSection() {
             Abitop
           </KumarText>
           <button onClick={handleOpenMenu}>
-            <img aria-hidden={true} src="/src/assets/menu.svg" />
+            <img aria-hidden={true} src="public/assets/menu.svg" />
           </button>
         </div>
         <div
@@ -72,7 +72,7 @@ function NavigationSection() {
           <div className="flex justify-between px-8 py-6">
             <div />
             <button onClick={handleCloseMenu} className="">
-              <img aria-hidden={true} src="/src/assets/cancel.svg" />
+              <img aria-hidden={true} src="public/assets/cancel.svg" />
             </button>
           </div>
           <Divider />
